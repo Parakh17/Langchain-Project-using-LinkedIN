1dbe3f31278372a28533a842eea13445fe40afea
Parakh Singhal <parakhsinghal2000@gmail.com>
2024-11-14 18:58:06 +0530

    push

--- tool/tools.py
+++ tool/tools.py
@@ -1,8 +1,14 @@
+#Tavily - 
 from langchain_community.tools.tavily_search import TavilySearchResults
-
+from dotenv import load_dotenv
+load_dotenv()
 
 def get_profile_url_tavily(name:str):
     search = TavilySearchResults()
     res = search.run(f"{name}")
     
     return res[0]["url"]
+
+
+x = get_profile_url_tavily("Salman")
+print(x)
