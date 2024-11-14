25fc3e467c0c2693e7aa03a9ab37bd1a265590b8
Parakh Singhal <parakhsinghal2000@gmail.com>
2024-11-14 23:03:31 +0530

    push

--- icebreaker.py
+++ icebreaker.py
@@ -10,9 +10,11 @@ load_dotenv()
 
 def ice_break_with(name:str) -> str:
     linkedin_url = linkedin_lookup_agent(name=name)
-    print("*"*10,"\nlinkedin url",linkedin_url)
+    print("*"*50,"\nlinkedin url",linkedin_url)
+    
     linkedin_url = "https://gist.githubusercontent.com/emarco177/0d6a3f93dd06634d95e46a2782ed7490/raw/78233eb934aa9850b689471a604465b188e761a0/eden-marco.json"
-    print("*"*10,"\nlinkedin url",linkedin_url)
+    print("*"*50,"\nlinkedin url",linkedin_url)
+    
     linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_url)
 
     summary_template = """
@@ -20,15 +22,22 @@ def ice_break_with(name:str) -> str:
             1. A short summary
             2. two interesting facts about them 
             
-        and output them in a json format with key as:
-        key 1 - short_summary
-        key 2 - facts
+        Use information 
+        \n{format_instructions}
+        
     
     """
+    
+    # and output them in a json format with key as:
+    #     key 1 - short_summary
+    #     key 2 - facts
+    
+    
     summary_prompt_template = PromptTemplate(
         input_variables=["information"],
         template=summary_template
     )
+    
     llm = ChatOpenAI(temperature=0, model="gpt-4o-mini")
     
     #chain = LLMChain(llm =llm, prompt = summary_prompt_template)
